75598c563b272795f9e538d3b59aa2f42177c584
Fahmin Mohammed <kkfahmin@gmail.com>
2025-05-29 13:52:24 +0530

    Made the base for other pjts as well as Update dependencies and enhance global styles with new color variables and animations

---
+++ components/Header.tsx
@@ -0,0 +1,44 @@
+"use client"
+import Link from "next/link"
+import AgentPulse from "@/components/AgentPulse"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { SignInButton } from "@clerk/nextjs"
+import { Button } from "./ui/button"
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 left-0 right-0 px-4 md:px-0 bg-white/80 backdrop-blur-sm border-b border-gray-200 z-50">
+     {/* left */}
+     <div className="container mx-auto ">
+      <div className="flex items-center justify-between h-16 px-3">
+      <div className="flex items-center justify-between h-16">  
+        <Link href="/" className="flex items-center gap-4">
+            <AgentPulse/>
+          <h1 className="text-xl font-semibold bg-gradient-to-r from-blue-600 to-blue-400 text-transparent bg-clip-text">
+            AgentTube
+          </h1>
+        </Link>
+      </div>
+      {/* right */}
+      <div className="flex items-center gap-4">
+        <SignedIn>
+            <Link href="/manage-plan" className="text-sm font-medium text-gray-500">
+            <Button variant="outline">Manage Plan</Button>
+            </Link>
+            <div className="ml-4">
+            <UserButton/>
+            </div>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton>
+            <Button variant="ghost" className="bg-gradient-to-r from-blue-600 to-blue-400 text-transparent bg-clip-text">Login</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+      </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
